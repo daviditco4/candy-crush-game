fa2551dc4ec40b1b10ae839f893cf81a2462b35a
supermassimo <massimocantugarcia@gmail.com>
2020-12-04 21:45:15 -0300

    Merged HorizontalStripedCandy and VerticalStripedCandy as children of StripedCandy

---
+++ src/game/backend/element/StripedCandy.java
@@ -0,0 +1,43 @@
+package game.backend.element;
+
+import game.backend.move.Direction;
+
+public class StripedCandy extends Candy {
+
+    private Orientation orientation;
+    public enum Orientation {
+        HORIZONTAL("HORIZ", Direction.LEFT, Direction.RIGHT),
+        VERTICAL("VERT", Direction.DOWN, Direction.UP);
+        String key;
+        Direction[] explosion;
+        Orientation (String key, Direction explosion0, Direction explosion1){
+            this.key = key;
+            explosion = new Direction[]{explosion0,explosion1};
+        }
+    }
+
+    public StripedCandy(Orientation orientation) {
+        this.orientation = orientation;
+    }
+
+    @Override
+    public String getKey() {
+        return orientation.key+"-STRIPED-" + super.getKey();
+    }
+
+    @Override
+    public String getFullKey() {
+        return orientation.key+"-STRIPED-" + super.getFullKey();
+    }
+
+    @Override
+    public Direction[] explode() {
+        return orientation.explosion;
+    }
+
+    @Override
+    public long getScore() {
+        return 80;
+    }
+
+}
