baf3e0301cc286f4f007a1b67a5c29d6ac7f2412
ValentinYeLi <67807505+ValentinYeLi@users.noreply.github.com>
2020-12-06 17:03:33 -0300

    GetCellsUnColored

--- src/game/backend/level/Level1.java
+++ src/game/backend/level/Level1.java
@@ -1,19 +1,28 @@
 package game.backend.level;
 
+import game.backend.Grid;
 import javafx.scene.paint.Color;
 
 public class Level1 extends Level0 {
+
+    private int acum;
 
     @Override
     public boolean tryMove(int y1, int x1, int y2, int x2) {
         if(super.tryMove(y1, x1, y2, x2)){
             if(y1 == y2){
                 for(int x=0 ; x < SIZE ; x++){
-                    getCell(y1, x).setColor(Color.YELLOW);
+                    if(getCell(y1, x).getColor() == null){
+                        getCell(y1, x).setColor(Color.YELLOW);
+                        acum++;
+                    }
                 }
             } else {
                 for(int y=0 ; y < SIZE ; y++){
-                    getCell(y, x1).setColor(Color.YELLOW);
+                    if(getCell(y, x1).getColor() == null){
+                        getCell(y, x1).setColor(Color.YELLOW);
+                        acum++;
+                    }
                 }
             }
             wasUpdated();
@@ -21,5 +30,11 @@ public class Level1 extends Level0 {
         }
         return false;
     }
+
+
+    public String CellsUnColored(){
+        return String.format("cells remaining: %d", Grid.SIZE * Grid.SIZE - acum);
+    }
+
 
 }
